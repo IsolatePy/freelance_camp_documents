72cb7cd75b35d4ec222f7b32c6c65445fc50b931
ali <ali.arb74@gmail.com>
2019-12-04 14:30:51 +0330

    Implemented documents API

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    file_url: "https://file-examples.com/wp-content/uploads/2017/02/file-sample_500kB.docx",
+    image_url: "https://lipsum.com/images/banners/black_250x250.gif",
+  )
+end
